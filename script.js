5b00f9a6c4bd2e7fbb27f158961caca9049c365a
jspong0 <jspong0@>
2020-07-22 12:14:04 +0300

    Fleshing out UI

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-$('#begin').on("click",function(e) {
-	$('#step-1').addClass('d-none')
-	$('#step-2').removeClass('d-none')
+let step = 3;
+setStep(step);
+
+$('button.next-step').on('click',function(e){
+step++;
+setStep(step)
 })
+
+function setStep(step){
+	$('.step').addClass('d-none')
+	$('#step-'+step).removeClass('d-none')
+}
