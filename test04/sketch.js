be5cb7832eb007db2d30ea8d81ad2cd81620d48e
jspong0 <jspong0@>
2020-07-28 16:07:35 +0300

    sound waves

--- test04/sketch.js
+++ test04/sketch.js
@@ -1,7 +1,10 @@
-let colors, ball;
+let maxRadius = 100;
+let phase = 1.01;
+let wHistory = [];
 
-let sample;
-let fft;
+let sample, fft;
+let modelShape;
+let status;
 
 let audioSamples = [
 	'../assets/sounds/BabyElephantWalk60.wav',
@@ -15,84 +18,101 @@ let audioSamples = [
 	'../assets/sounds/taunt.wav',
 ];
 
-let loadSampleButton;
-let stopPlaybackButton;
-let monitorPlaybackButton;
+class Tweak {
+	constructor() {
+		this.maxRadius = 100;
+		this.resolution = 16; // fft bins
+		this.phaseShift = 0.001;
+		this.loadSample = loadSample;
+		this.stopPlayback = stopPlayback;
+		this.startPlayback = startPlayback;
+		this.monitorPlayback = monitorPlayback;
+	}
+}
 
-let modelShape;
-
-let status;
-
-let maxRadius = 100;
-let bins = 16;
-let phase = 1.01;
+class Color {
+	constructor() {
+		this.h = random(360);
+		this.s = random(100);
+		this.b = random(70, 90);
+	}
+}
 
 function setup() {
 	let canvas = createCanvas(852, 600);
 	canvas.parent('canvas-container');
 
-	status = select('#status');
-
-	fft = new p5.FFT();
+	// Reference grid
+	// textSize(10)
+	// noFill()
+	// for (let x = 0; x < width; x += 25) {
+	// 	stroke(255,100)
+	// 	line(x,0,x,height)
+	// 	stroke('white')
+	// 	text(x,x-2,12)
+	// }
+	// for (let y = 0; y < height; y += 25){
+	// 	stroke(255,100)
+	// 	line(0,y,width,y)
+	// 	stroke('white')
+	// 	text(y,2,y+2)
+	// }
 
 	modelShape = [
-		// createVector(300, 270),
-		createVector(300, 250),
-		createVector(350, 200),
-		createVector(500, 200),
-		createVector(550, 250),
-		createVector(450, 300),
-		createVector(300, 250),
-		// createVector(300, 100),
+		createVector(225, 250),
+		createVector(325, 200),
+		createVector(535, 200),
+		createVector(635, 250),
+		createVector(width / 2, 300),
 	];
-	console.table(modelShape);
-
-	loadSampleButton = select('#load-sample');
-	loadSampleButton.mousePressed(loadSample);
-	stopPlaybackButton = select('#stop-playback');
-	stopPlaybackButton.mousePressed(stopPlayback);
-	monitorPlaybackButton = select('#monitor-playback');
-	monitorPlaybackButton.mousePressed(monitorPlayback);
 
 	colors = new Color();
+	tweak = new Tweak();
+	fft = new p5.FFT();
 
 	let gui = new dat.GUI();
+	let sketchControlsFolder = gui.addFolder('Sketch controls');
+	let sampleContentFolder = gui.addFolder('Sample content');
 
-	gui.add(colors, 'h', 0, 360);
-	gui.add(colors, 's', 0, 100);
-	gui.add(colors, 'b', 0, 100);
+	sampleContentFolder.add(tweak, 'loadSample');
+	sampleContentFolder.add(tweak, 'startPlayback');
+	sampleContentFolder.add(tweak, 'stopPlayback');
+	sampleContentFolder.add(tweak, 'monitorPlayback');
+
+	sketchControlsFolder.add(tweak, 'resolution', 16, 1024, 16);
+
+	sampleContentFolder.open();
+	sketchControlsFolder.open();
+
+	status = select('#status');
 
 	loadSample();
 }
 
 function draw() {
+	background(50);
 	noFill();
 	stroke('white');
 	strokeWeight(1);
-
-	push();
-	colorMode(HSB);
-	background(colors.h, colors.s, colors.b);
-	pop();
 
 	// Draw model shape for reference
 	beginShape();
 	modelShape.forEach(p => {
 		vertex(p.x, p.y);
 	});
-	endShape();
+	endShape(CLOSE);
 
 	if (sample) {
 		if (sample.isPlaying()) {
 			fft.setInput(sample);
 
-			let spectrum = fft.analyze(bins);
-			// console.log(spectrum)
+			// Make sure the number of bins we give fft is a power of 2 (even though we can input any value in dat.gui)
+			let spectrum = fft.analyze(pow(2, ceil(log(tweak.resolution) / log(2))));
 
-			let step = width / bins;
-			status.html(step);
+			let step = width / tweak.resolution;
 
-			for (let i = 0; i < bins; i++) {
+			// first loop to draw references
+			for (let i = 0; i < tweak.resolution; i++) {
 				let f = spectrum[i];
 				let x = i * step;
 				let y = height - f;
@@ -112,40 +132,34 @@ function draw() {
 				pop();
 			}
 
-			push();
-			stroke(100)
-			strokeWeight(20)
+			// second loop to draw shapes
+
+			strokeWeight(2);
 			beginShape();
-			for (let i = 0; i < bins; i++) {
+			for (let i = 0; i < tweak.resolution; i++) {
 				let f = spectrum[i];
-
-				let x = i * step;
-				let y = height - f;
-
-				// Assign points on the shape (face) to fragments of the wave length
-				// let pIndex = round(i % modelShape.length);
-				let pIndex = round(map(i, 0, bins, 0, modelShape.length - 1));
-				let p = modelShape[pIndex];
-
-				let v = map(i, 0, bins, 0, 360);
-
-				translate(p.x, p.y);
-				let noiseMax = 1;
-				for (let a = 0; a < TWO_PI; a += radians(26)) {
-					let xoff = map(cos(a + phase), -1, 1, 0, noiseMax);
-					let yoff = map(sin(a + phase), -1, 1, 0, noiseMax);
-					let r = map(noise(xoff, yoff, zoff), 0, 1, f / 2, f);
-					let x = r * cos(a);
-					let y = r * sin(a);
-					point(x, y);
-					// console.log(x,y)
-				}
+				blob(width / 2, height / 2, 100);
 			}
 			endShape(CLOSE);
-			pop();
-			
+
 			let waveform = fft.waveform();
-			// console.log(waveform)
+
+			// Third loop to draw waveform
+			wHistory.push(waveform);
+			push();
+			let cY = map(waveform, 0, 1, height, 0);
+			translate(0, height / 2 - cY);
+			beginShape();
+			for (let i = 0; i < wHistory.length; i++) {
+				let y = map(wHistory[i], 0, 1, height, 0);
+				vertex(i, y);
+			}
+			endShape();
+			pop();
+			if (wHistory.length > width - 50) {
+				wHistory.splice(0, 1);
+			}
+			line(wHistory.length, 0, wHistory.length, height);
 
 			phase += 0.01;
 		}
@@ -170,19 +184,33 @@ function blob(posX, posY, radius) {
 		let r = map(noise(xoff, yoff, zoff), 0, 1, radius, height / 2);
 		let x = r * cos(a);
 		let y = r * sin(a);
-		vertex(x, y);
+		curveVertex(x, y);
+		xoff += 0.0000001;
 	}
 	endShape(CLOSE);
-	phase += 0.003;
-	zoff += 0.01;
+	phase += 0.00004;
+	zoff += 0.000001;
 
 	pop();
-}
 
-function Color() {
-	this.h = random(360);
-	this.s = random(100);
-	this.b = random(70, 90);
+	beginShape();
+	let i = 0;
+	for (let a = 0; a < TWO_PI; a += ang) {
+		i++;
+		let sx = x + cos(a) * rad2;
+		let sy = y + sin(a) * rad2;
+		console.log('sx' + sx);
+		console.log('sy' + sy);
+		fill(72, 139, 143);
+		curveTightness(-0.5);
+		curveVertex(sx, sy);
+
+		sx = x + cos(a + halfAng) * randomCornerLengths[i];
+		sy = y + y + sin(a + halfAng) * randomCornerLengths[i];
+
+		curveVertex(sx, sy);
+	}
+	endShape(CLOSE);
 }
 
 function loadSample() {
@@ -199,6 +227,9 @@ function sampleReady() {
 
 function stopPlayback() {
 	sample.stop();
+}
+function startPlayback() {
+	sample.play();
 }
 
 function monitorPlayback() {
